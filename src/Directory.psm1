b8c85d75ed420c6f3cd629eb18ee0e61ee4c8865
speyck <54800864+speyck@users.noreply.github.com>
2022-03-17 11:52:41 +0100

    Rename Directory.ps1 to Directory.psm1

---
+++ src/Directory.psm1
@@ -0,0 +1,54 @@
+# Import Directory class
+using module ".\DirectoryTreeView.psm1"
+
+class Directory {
+
+    [String]$Path
+    [String]$Name
+    [Double]$Size
+    [System.Collections.Generic.IEnumerable[Directory]]$Children = $null; 
+
+    Directory([String]$path, [Bool]$getChildren = $true) {
+        $this.CheckIsFolder($path)
+
+        $this.Path = $path
+        
+        $this.Size = $this.GetFolderSize($path)
+
+        if ($getChildren) {
+            $this.GetChildren
+        }
+    }
+
+    Directory([String]$path, [Double]$size, [System.Collections.Generic.IEnumerable[Directory]]$children) {
+        $this.Path = $path;
+        $this.Size = $size;
+        $this.Cildren = $children;
+    }
+
+    [void] GetChildren() {
+
+    }
+
+    [double] GetFolderSize([string]$path) {
+        $this.CheckIsFolder($path)
+        Get-ChildItem $path -Recurse | Out-GridView
+        return (Get-ChildItem $path -Recurse | Measure-Object -Property Length -Sum).Sum
+    }
+
+    [Void] CheckIsFolder([string]$path) {
+        if (!(Test-Path -Path $path -PathType Container)) {
+            throw "Directory does not exist"
+        }
+    }
+
+    <#. 
+        .DESCRIPTION
+        Displays the Directory object
+    .#>
+    [void]Display([bool]$includeChildren = $true) {
+        $treeView = [DirectoryTreeView]::New($this)
+
+        $treeView.ShowDialog()
+    }
+}
